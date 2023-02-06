ba0bec2c5ba24048d467ebbdbb6924ed0efbe5ca
Amit Vashist <amitvashist7@outlook.com>
2023-02-06 13:02:06 +0000

    23-Backend-and-RemoteState

---
+++ 23-Backend-and-RemoteState/Local/solution/kuldeep/terraform.tf
@@ -0,0 +1,13 @@
+terraform { 
+  required_providers { 
+   azurerm = {
+     source = "hashicorp/azurerm"
+     #version = "=2.97.0"
+   }
+ }
+
+  backend "local" { 
+   path = "/var/tmp/terraform-backend-local-demo/terraform.tfstate"
+  }
+ 
+}
